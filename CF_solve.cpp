c28610a9caceec8bd0f63902b1df38376d2c6d3d
Rakibul-Hasan-Konok <rakibulhasankonok.ju@gmail.com>
2022-12-18 21:46:23 +0600

    Codeforce_Solution_Div_3

---
+++ CF_solve.cpp
@@ -0,0 +1,21 @@
+//link-https://codeforces.com/contest/1772/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--){
+
+   string s;
+   cin>>s;
+   int n=s[0]-'0'+s[2]-'0';
+   cout<<n<<endl;
+
+    }
+   
+
+}
