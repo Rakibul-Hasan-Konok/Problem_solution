3fa4fcc0fb072dd53bfa8f18ea4e57fee0bbd896
Rakibul-Hasan-Konok <rakibulhasankonok.ju@gmail.com>
2022-12-19 22:34:58 +0600

    lab_workH

---
+++ OOP_LabH.cpp
@@ -0,0 +1,14 @@
+
+//link-https://vjudge.net/contest/534298#problem/H
+#include<bits/stdc++.h>
+using namespace std;
+int main( )
+{
+    char ch;
+    cin >> ch;
+    if(ch == 'A'|| ch== 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+        cout << "Vowel"<<endl;
+    else
+        cout << "Consonant"<<endl;
+    return 0;
+}
