5edb7c5e4ea0a7e5801471f77e5790309231a1ee
Rakibul-Hasan-Konok <rakibulhasankonok.ju@gmail.com>
2022-12-30 09:23:16 +0600

    lab

--- Hlw.cpp
+++ Hlw.cpp
@@ -1,10 +1,12 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 
 int main()
 {
-
-
-
+int t;
+cin>>t;
+for(int i=1;i<=t;i++){
+    
+}
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin>>s;
+     cout<<(char) toupper(s[0]);
+    for(int i=1;i<=s.size();i++){
+        if(s[i]=='s') cout<<"$";
+        else if(s[i]=='i') cout<<"!";
+        else if(s[i]=='o') cout<<"()";
+         else cout<<s[i];
+    }
+    cout<<"."<<endl;
+}
