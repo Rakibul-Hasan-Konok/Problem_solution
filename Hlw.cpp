6ba60edf6105dee811ef829ace73138d0cd2fd31
Rakibul-Hasan-Konok <rakibulhasankonok.ju@gmail.com>
2022-12-31 21:37:22 +0600

    lab_wrk

--- Hlw.cpp
+++ Hlw.cpp
@@ -1,16 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main( )
+
+int main()
 {
-    long long int t, n, x, y;
-    cin >> t;
-    while(t--)
-    {
-        cin >> n >> x>> y;
-        if(((x+y) < n - 1)||(x == n && y == n))
-            cout << "Yes\n";
-        else
-            cout << "No\n";
+   
+    int n;
+   
+    cin>>n;
+    vector<long long>a(n);
+    long long int total=0;
+    
+    long long int ans = LONG_LONG_MAX;
+    for(int i = 0 ; i < n ; i++)
+    { 
+        cin>>a[i];
+        total+=a[i];
     }
+    for(int mask=0; mask< (1<<n); mask++)
+    {
+        long long int sum=0;
+        for(int i=0; i<n; i++)
+        {
+            if(mask>>i & 1) sum+=a[i];
+        }
+        long long int diff=abs((total-sum)-sum);
+        ans=min(ans,diff);
+    }
+    cout<<ans<<endl;
+
     return 0;
 }
